cf1e27de6db2ac537099abba0d67829deaa7cd2f
Sergey Marin <marin.sergey@gmail.com>
2011-10-27 11:53:44 +0400

    initial commit

---
+++ PluginSisyphus.class.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Copyright © 2011 Sergey Marin
+ *
+ * Плагин Sisyphus: защита данных в HTML формах с помощью jQuery плагина Sisyphus.js
+ * Автор: Sergey Marin
+ * Профиль: http://livestreet.ru/profile/HangGlider/
+ * Сайт: http://sergeymarin.com
+ *
+ * jQuery плагин Sisyphus:
+ * Автор: Alexander Kaupanin
+ * Email: kaupanin@gmail.com
+ * Сайт: https://github.com/simsalabim/sisyphus
+ *
+ * GNU General Public License, version 2:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ */
+if (!class_exists('Plugin')) {
+    die('Hacking attemp!');
+}
+class PluginSisyphus extends Plugin {
+    /**
+     * Активация плагина
+     * @return boolean
+     */
+	public function Activate() {
+
+	}
+
+    /**
+     * Инициализация плагина
+     * @return void
+     */
+	public function Init() {
+
+	}
+
+    /**
+     * Деактивация плагина
+     * @return boolean
+     */
+	public function Deactivate() {
+
+	}
+}
